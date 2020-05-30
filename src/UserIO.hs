8b46a0775332aaf1dd626a077371d0b8e7e3cfc8
Marcus Plutowski <achierius@berkeley.edu>
2020-05-30 20:43:07 +0000

    Re-add comments for readability
    TODO: Figure out commenting standard to use
    Currently using a weird indentation hierarchy thing to denote relations

--- src/UserIO.hs
+++ src/UserIO.hs
@@ -11,22 +11,35 @@ import qualified Data.Bifunctor as Bifunctor
 import qualified Control.Exception as Exception
 import           Options.Applicative
 import           Text.RegexPR
+-- TODO: Migrate to regex-pcre instead
 
+-- data
+
+  -- Note: each CSV line entry is ordered as follows:
+  --    ENGLISH,/IPA/
+  -- this regex uses lookbehind/lookahead to extract the IPA
 engIPALoc = "dist/resources/en/en_US.csv"
 engIPARegex = "(?<=/).+(?=/)"
 regDflt = (("", ("", "")), [])
+-- TODO: Make regDflt a function String -> (that tuple thing)
 
 englishVowels = ['ɑ', 'ɪ', 'ə', 'ɔ', 'æ', 'ʒ', 'ʊ', 'i', 'u', 'e']
 engVowelsRegx = "[" ++ englishVowels ++ "]"
+
+-- types
 
 newtype IPAString = IPAString String deriving (Eq)
 
 data Options = Options
     { oStdIn :: Bool, oTarget :: Maybe String }
 
+-- core logic
+
 runProgram :: Options -> IO ()
 runProgram o =
     putStrLn =<< getIPALine o
+
+  -- lookup from IPA dictionary
 
 getIPALine :: Options -> IO String
 getIPALine o = do
@@ -37,12 +50,16 @@ getIPALine o = do
     let capturedIPA = fromMaybe regDflt (matchRegexPR engIPARegex line)
     return (fst . fst $ capturedIPA)
 
+-- tutorial code for handling CLI and file IO
+
 loadInputInfo :: Options -> IO (Either String String)
 loadInputInfo o =
     maybe defaultResponse readFileFromOptions $ oTarget o
   where
     readFileFromOptions f = Bifunctor.first show <$> safeReadFile f
     defaultResponse = return $ Right "Default Response?"
+
+  -- CLI
 
 parseCLI :: IO Options
 parseCLI = execParser (withInfo parseOptions "File Fun")
@@ -53,6 +70,8 @@ parseOptions :: Parser Options
 parseOptions = Options
     <$> (switch $ long "stdin")
     <*> (optional $ strOption $ long "file")
+
+  -- safer reading of files
 
 safeReadFile :: FilePath -> IO (Either Exception.IOException String)
 safeReadFile = Exception.try . readFile
